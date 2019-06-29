19677d1be7c8599998adc1879416c1b4fbb5f7bf
schindamo <51295966+schindamo@users.noreply.github.com>
2019-06-29 16:21:47 -0700

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,14 @@
+# guessing game.
+
+secret_word = "giraffe"
+guess = ""
+count = 0
+
+while guess != secret_word:
+    guess = input("Enter your guess: ")
+    count += 1
+
+if guess == secret_word and count == 0:
+    print("You won in one guess!")
+else:
+    print("You won in " + str(count) + " guesses!")
